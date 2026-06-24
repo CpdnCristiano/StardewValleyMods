112f1840b1e06abde07b95e7f95a2743425cc36e
Cristiano Nascimeno <Cristiano@proserv.net.br>
2026-06-24 08:35:34 -0300

    add decompiler to ignore

--- FullInventoryView/Patcher/InventoryMenuPatcher.cs
+++ FullInventoryView/Patcher/InventoryMenuPatcher.cs
@@ -14,6 +14,7 @@ using StardewModdingAPI;
 using StardewValley;
 using StardewValley.Inventories;
 using StardewValley.Menus;
+using CpdnCristiano.StardewValleyMod.FullInventoryView.Framework;
 using static StardewValley.Menus.InventoryMenu;
 
 namespace CpdnCristiano.StardewValleyMod.FullInventoryView.Patcher
@@ -1925,67 +1926,6 @@ namespace CpdnCristiano.StardewValleyMod.FullInventoryView.Patcher
                 : GridViewports.TryGetValue(inventoryMenu, out var existing) ? existing
                 : null;
             return state is not null;
-        }
-
-        private sealed class ScrollableInventoryList : IList<Item>
-        {
-            private readonly int _offset;
-            private readonly int _capacity;
-
-            public ScrollableInventoryList(IList<Item> underlying, int offset, int capacity)
-            {
-                Underlying = underlying;
-                _offset = offset;
-                _capacity = capacity;
-            }
-
-            public IList<Item> Underlying { get; }
-
-            public Item this[int index]
-            {
-                get => Underlying[_offset + index];
-                set => Underlying[_offset + index] = value;
-            }
-
-            public int Count => Math.Max(0, Math.Min(_capacity, Underlying.Count - _offset));
-            public bool IsReadOnly => Underlying.IsReadOnly;
-
-            public void Add(Item item) => throw new NotSupportedException();
-
-            public void Clear() => throw new NotSupportedException();
-
-            public bool Contains(Item item) => IndexOf(item) >= 0;
-
-            public void CopyTo(Item[] array, int arrayIndex)
-            {
-                for (int i = 0; i < Count; i++)
-                    array[arrayIndex + i] = this[i];
-            }
-
-            public IEnumerator<Item> GetEnumerator()
-            {
-                for (int i = 0; i < Count; i++)
-                    yield return this[i];
-            }
-
-            public int IndexOf(Item item)
-            {
-                for (int i = 0; i < Count; i++)
-                {
-                    if (Equals(this[i], item))
-                        return i;
-                }
-                return -1;
-            }
-
-            public void Insert(int index, Item item) => throw new NotSupportedException();
-
-            public bool Remove(Item item) => throw new NotSupportedException();
-
-            public void RemoveAt(int index) => throw new NotSupportedException();
-
-            System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() =>
-                GetEnumerator();
         }
     }
 }
